b2d772d688476145fa7ae7b8b8e2ffbd6ccae5d9
Malte-Christian Neumann <mneuman2@hs-heilbronn.de>
2020-05-25 20:47:52 +0200

    Add Game Manager to Scene

---
+++ ElementMysteries/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ElemantMysteries
+{
+    public class GameManager : MonoBehaviour
+    {
+        [Header("Static Attributes")]
+        public static GameManager instance;
+
+        void Awake()
+        {
+
+            //Singelton Setup
+            if (instance != null)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+    }
+}
